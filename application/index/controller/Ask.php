578fa7f99c7f81ac404917ac3839f56abed6efd6
shanemiracle <824116664@qq.com>
2017-01-10 11:19:19 +0800

    1

---
+++ application/index/controller/Ask.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoj
+ * Date: 2017/1/10
+ * Time: 10:26
+ */
+
+namespace app\index\controller;
+
+
+class Hello
+{
+    public function index( $name) {
+        $ret = '';
+        $client = new swoole_client(SWOOLE_SOCK_TCP);
+
+        if( $client ) {
+            $ret = 'yes';
+        }
+        else {
+            $ret = 'no';
+        }
+        return $ret;
+    }
+}

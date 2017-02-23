f46c034ea79307b33849a7e420ce380e64a315a4
shanemiracle <824116664@qq.com>
2017-02-23 14:17:35 +0800

    1

--- application/index/controller/Hospital.php
+++ application/index/controller/Hospital.php
@@ -9,11 +9,36 @@
 namespace app\index\controller;
 
 
+use app\index\api\apiHospital;
+use think\controller\Rest;
+use think\Session;
 use think\View;
 
-class Hospital
+class Hospital extends Rest
 {
     public function index() {
-        return (new View())->fetch('/hospital/index');
+        $total_num = 0;
+        $attest = Session::get('attest');
+        $retData = apiHospital::apiHospitalGet($attest,0,1);
+        if( $retData ) {
+            if( $retData['ret_code'] == 0 ) {
+                $total_num = $retData['total_num'];
+            }
+        }
+
+        return (new View())->fetch('/hospital/index',['total_num'=>$total_num]);
+    }
+
+    public  function ajax_list() {
+        $data = [];
+        $attest = Session::get('attest');
+        $retData = apiHospital::apiHospitalGet($attest,0,100);
+        if( $retData ) {
+            if( $retData['ret_code'] == 0 ) {
+                $data = $retData['data'];
+            }
+        }
+
+        return $this->response(['data'=>$data],'json',200);
     }
 }

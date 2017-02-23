31ccbe5f98e114cd06625998a8ba4a1659934965
qiumei527 <736844923@qq.com>
2017-02-23 14:46:35 +0800

    hospital
    hospital

---
+++ application/index/controller/Doctor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2017/2/23
+ * Time: 14:45
+ */
+
+namespace app\index\controller;
+
+
+class Doctor
+{
+
+}
--- application/index/controller/Hospital.php
+++ application/index/controller/Hospital.php
@@ -38,7 +38,95 @@ class Hospital extends Rest
                 $data = $retData['data'];
             }
         }
-
         return $this->response(['data'=>$data],'json',200);
     }
+
+
+    public  function ajax_add() {
+        $data = [];
+        $attest = Session::get('attest');
+        $hospital_name = Request::instance()->param('hospital_name');
+        $hospital_number = Request::instance()->param('hospital_number');
+        $zone = Request::instance()->param('zone');
+        $logo = Request::instance()->param('logo');
+        $level = Request::instance()->param('level');
+
+        $retData = apiHospital::apiHospitalAdd($attest,$hospital_name,$hospital_number,$zone,$logo,$level);
+        if( $retData ) {
+            if( $retData['ret_code'] == 0 ) {
+                print 0;
+            }
+            else {
+                print retData['ret_code'];
+            }
+        }
+        print 10000;
+    }
+
+    public  function ajax_set() {
+        $data = [];
+        $attest = Session::get('attest');
+        $hospital_no = Request::instance()->param('hospital_no');
+        $hospital_ver = Request::instance()->param('hospital_ver');
+        $hospital_name = Request::instance()->param('hospital_name');
+        $zone = Request::instance()->param('zone');
+        $logo = Request::instance()->param('logo');
+        $level = Request::instance()->param('level');
+
+        $retData = apiHospital::apiHospitalSet($attest,$hospital_no,$hospital_ver,$zone,$hospital_name,$logo,$level);
+        if( $retData ) {
+            if( $retData['ret_code'] == 0 ) {
+                print 0;
+            }
+            else {
+                print retData['ret_code'];
+            }
+        }
+        print 10000;
+    }
+
+    public  function ajax_del() {
+        $data = [];
+        $attest = Session::get('attest');
+        $hospital_no = Request::instance()->param('hospital_no');
+
+        $retData = apiHospital::apiHospitalDrop($attest,$hospital_no);
+        if( $retData ) {
+            if( $retData['ret_code'] == 0 ) {
+                print 0;
+            }
+            else {
+                print retData['ret_code'];
+            }
+        }
+        print 10000;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

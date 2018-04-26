5b5f4a3d1f1d21476c21ac88890e201450a6cdc4
jinjidecoco <18895627215@163.com>
2018-04-26 09:51:54 +0800

    代码优化coco

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,34 +11,56 @@ use think\View;
 
 class Index
 {
-    public function index()
+    public function login()
     {
-       // return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
         $user_name = Request::instance()->param('user_name');
         $pwd = Request::instance()->param('pwd');
-
-        if($user_name == null || $pwd == null ) {
-            $user_name = Session::get('user_name', $user_name);
-            $pwd = Session::get('user_name', $pwd);
-        }
 
         $recv = apiLogin::apiLogin($user_name,$pwd);
         if( $recv ) {
             if($recv['ret_code'] == 0) {
                 Session::set('user_name', $user_name);
-                Session::set('pwd', $pwd);
                 Session::set('attest',$recv['attest']);
-                $data = ['user_name'=>$user_name];
-                return (new View())->fetch('index',$data);
             }
-            if($recv['ret_code'] == 6){
-                abort(401,'未登录');
-            }
+            // if($recv['ret_code'] == 6){
+            //     abort(401,'未登录');
+            // }
         }
 
+        return $recv;
+
+    }
+    
+    public function index()
+    {
+       // return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        // $user_name = Request::instance()->param('user_name');
+        // $pwd = Request::instance()->param('pwd');
+
+        // if($user_name == null || $pwd == null ) {
+        //     $user_name = Session::get('user_name', $user_name);
+        //     $pwd = Session::get('user_name', $pwd);
+        // }
+
+        // $recv = apiLogin::apiLogin($user_name,$pwd);
+        // if( $recv ) {
+        //     if($recv['ret_code'] == 0) {
+        //         Session::set('user_name', $user_name);
+        //         Session::set('pwd', $pwd);
+        //         Session::set('attest',$recv['attest']);
+        //         $data = ['user_name'=>$user_name];
+        //         return (new View())->fetch('index',$data);
+        //     }
+        //     if($recv['ret_code'] == 6){
+        //         abort(401,'未登录');
+        //     }
+        // }
 
 
-        return (new View())->fetch('login/index');
+
+        // return (new View())->fetch('login/index');
+           return (new View())->fetch('index');
+
 
     }
 
